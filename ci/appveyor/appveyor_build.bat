f172a5de7bc1e96d253e5000064827602e8a1f13
micbou <contact@micbou.com>
2018-02-19 02:55:56 +0100

    [WIP] Test RestartServer java test

--- ci/appveyor/appveyor_build.bat
+++ ci/appveyor/appveyor_build.bat
@@ -1,1 +1,1 @@
-python run_tests.py --no-flake8 --runs 20 --completers java -- ycmd\tests\java\server_management_test.py
+python run_tests.py --no-flake8 --runs 20 --completers java -- ycmd\tests\java\server_management_test.py:ServerManagement_RestartServer_test
