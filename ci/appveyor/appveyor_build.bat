e2814132503bac8999475c87cf0d2bd2475e6cb7
micbou <contact@micbou.com>
2019-04-06 15:09:23 +0200

    [WIP] Fix Java flakiness

--- ci/appveyor/appveyor_build.bat
+++ ci/appveyor/appveyor_build.bat
@@ -1,1 +1,1 @@
-python run_tests.py --no-flake8 --runs 10
+python run_tests.py --no-flake8 --runs 10 ycmd/tests/java
--- ci/travis/travis_script.sh
+++ ci/travis/travis_script.sh
@@ -1,1 +1,1 @@
-./run_tests.py --no-flake8 --runs 10
+./run_tests.py --no-flake8 --runs 5 ycmd/tests/java
