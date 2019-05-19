a1bf1db5fe1bb24381b1a1a1af99f642ae4c00b9
micbou <contact@micbou.com>
2019-05-19 18:08:39 +0200

    Check Poll_Diagnostics_ProjectWide_Eclipse_test flakiness

--- azure/windows/run_tests.bat
+++ azure/windows/run_tests.bat
@@ -7,4 +7,4 @@ set "PATH=C:\Go\bin;%PATH%"
 :: Prevent the already installed version of Go to conflict with ours.
 set GOROOT=
 
-python run_tests.py --msvc %MSVC% --runs 10
+python run_tests.py --msvc %MSVC% --runs 100 ycmd\tests\java\diagnostics_test.py:Poll_Diagnostics_ProjectWide_Eclipse_test
